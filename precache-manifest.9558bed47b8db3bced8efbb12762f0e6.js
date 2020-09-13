88d024fc7b968a522121311e9beaf2ef9361ed3c
Jojolele <ajbkn18@gmail.com>
2020-09-13 20:37:10 +0300

    Updates

---
+++ precache-manifest.9558bed47b8db3bced8efbb12762f0e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4fb7786d6cea68b3edacbc3d89c59c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7fad668b1aa9a25d9cf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2cbbf8610d63111db0be",
+    "url": "/static/css/main.b3c5627c.chunk.css"
+  },
+  {
+    "revision": "c7fad668b1aa9a25d9cf",
+    "url": "/static/js/2.e6116b70.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.e6116b70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cbbf8610d63111db0be",
+    "url": "/static/js/main.e630975d.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
